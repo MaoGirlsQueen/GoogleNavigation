4eb0bf68c3763e73de4419c71a8e15707de4a36c
cnwutianhao <cnwutianhao@163.com>
2017-05-06 00:52:21 +0800

    v1.0版

---
+++ app/src/main/java/com/tnnowu/android/googlenavigation/FirstActivity.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2017 Tnno Wu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tnnowu.android.googlenavigation;
+
+import android.os.Bundle;
+
+/**
+ * Created by Tnno Wu on 2017/5/6.
+ */
+
+public class FirstActivity extends BaseActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+        initializeToolbar();
+    }
+
+}
